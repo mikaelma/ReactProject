fefe931d1d73abaaaad5caa10014e8536cce4dfd
Hans Brenna <hans_brenna@hotmail.com>
2017-10-17 18:04:11 +0200

    Started work on note dialog

--- personal-planner/src/main.js
+++ personal-planner/src/main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MainPage from './screens/main-page';
+import ProfilePage from './screens/profile-page';
+import NotePage from './screens/note-page';
 
 
 const Main = () => (
