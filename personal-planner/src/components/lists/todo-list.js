fed30bac630f86568d4cbb98eb9d78881ca09253
Matz-Leander Solheim Wiik <matzwiik@gmail.com>
2017-10-17 19:31:05 +0200

    Fixed bug on checkbox and localstorage

--- personal-planner/src/components/lists/todo-list.js
+++ personal-planner/src/components/lists/todo-list.js
@@ -31,8 +31,10 @@ async componentWillMount(){
       elements[index].checked = !elements[index].checked;
       this.setState({
           elements: elements,
+      }, () => {
+          window.localStorage.setItem('todo_elements',JSON.stringify(this.state.elements));
       })
-  }
+  };
 
   handleFieldKeyDown = (e) => {
       switch (e.key){
